cbb734a6063e944ca9ef8699b1dfe8e1c8fa0d44
Brannon N. Darby II <bdarby86@gmail.com>
2018-06-21 07:16:36 -0400

    refactor: update contact info

--- src/app/components/contact/contact.ts
+++ src/app/components/contact/contact.ts
@@ -6,24 +6,47 @@ import { Meta, Title } from '@angular/platform-browser'
   template: `
     <h3>{{ header }}</h3>
     <div class="row">
-      <div class="col-2 text-right">
-        <i class="fa fa-fw fa-phone"></i>
+      <div class="col-md-4">
+        <div class="row">
+          <div class="col-2 text-right">
+            <i class="fa fa-fw fa-phone"></i>
+          </div>
+          <div class="col-10">
+            <a href="tel:+1-571-527-4935">+1-571-527-4935</a>
+          </div>
+          <div class="col-2 text-right">
+            <i class="fa fa-fw fa-envelope-o"></i>
+          </div>
+          <div class="col-10">
+            <a href="mailto:info@solaiya.com">info@solaiya.com</a>
+          </div>
+        </div>
       </div>
-      <div class="col-10">
-        <a href="tel:+1-571-527-4935">+1-571-527-4935</a>
+      <div class="col-md-4">
+        <div class="row">
+          <div class="col-2 text-right">
+            <i class="fa fa-fw fa-map-marker"></i>
+          </div>
+          <div class="col-10">
+            <p class="mb-0">SOLAIYA LLLP</p>
+            <p class="mb-0">4250 N Fairfax Dr.</p>
+            <p class="mb-0">Suite 600</p>
+            <p class="mb-0">Arlington, VA 22203</p>
+          </div>
+        </div>
       </div>
-      <div class="col-2 text-right">
-        <i class="fa fa-fw fa-envelope-o"></i>
-      </div>
-      <div class="col-10">
-        <a href="mailto:info@solaiya.com">info@solaiya.com</a>
-      </div>
-      <div class="col-2 text-right">
-        <i class="fa fa-fw fa-map-marker"></i>
-      </div>
-      <div class="col-10">
-        664 A South Military Trail
-        <br> Deerfield Beach, FL 33442
+      <div class="col-md-4">
+        <div class="row">
+          <div class="col-2 text-right">
+            <i class="fa fa-fw fa-map-marker"></i>
+          </div>
+          <div class="col-10">
+            <p class="mb-0">SOLAIYA CZ s.r.o.</p>
+            <p class="mb-0">Praha City Center</p>
+            <p class="mb-0">Klimentská 1216/46</p>
+            <p class="mb-0">Praha 1, 110 02</p>
+          </div>
+        </div>
       </div>
     </div>
   `,
@@ -31,7 +54,7 @@ import { Meta, Title } from '@angular/platform-browser'
 export class ContactComponent implements OnInit {
   public header: string
 
-  constructor(private meta: Meta, private title: Title) {}
+  constructor(private meta: Meta, private title: Title) { }
 
   ngOnInit() {
     this.header = 'Contact Us'
